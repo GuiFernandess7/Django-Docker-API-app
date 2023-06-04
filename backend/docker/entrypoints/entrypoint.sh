56ceee5e2f31e2e6752bfac424ee2317e98560a6
Guilherme Fernandes <guilhermefs09@protonmail.com>
2023-06-04 18:49:01 -0300

    app updated

--- backend/docker/entrypoints/entrypoint.sh
+++ backend/docker/entrypoints/entrypoint.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+
 python manage.py makemigrations
 python manage.py migrate
 python manage.py test
--- backend/drf_course/settings.py
+++ backend/drf_course/settings.py
@@ -34,7 +34,9 @@ INSTALLED_APPS = [
     'django_extensions',
     'django_filters', 
     'rest_framework', 
+    'rest_framework.authtoken',
     'core',
+    'ecommerce'
 ]
 
 MIDDLEWARE = [
@@ -125,6 +127,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework_json_api.parsers.JSONParser',
     ),
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework_json_api.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer'
